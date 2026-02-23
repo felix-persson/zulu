70aaea3132703bed69c6f7dc1918c6e6614fdbba
felix-persson <felix.persson@me.com>
2026-02-23 21:30:02 +0100

    Change default C flags

--- config.mk
+++ config.mk
@@ -1,7 +1,7 @@
 VERSION = 1.0
 
-CFLAGS = -g -std=c99 -pedantic -Wall -Werror -O0
-#CFLAGS = -std=c99 -pedantic -Wall -Werror -Wno-deprecated-declarations -Os
+#CFLAGS = -g -std=c99 -pedantic -Wall -Werror -O0
+CFLAGS = -std=c99 -pedantic -Wall -Werror -Wno-deprecated-declarations -Os
 
 CC = clang
 LEX = flex
